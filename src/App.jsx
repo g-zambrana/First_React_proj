ef85555dc7598ae0f180eaf6c6e5642b489ec71c
g.zambrana <gabrielz022604@gmail.com>
2026-04-06 12:14:50 -0400

    made custom component header, with dynamic data

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,26 @@
-function App() {
+import reactImg from './assets/react-core-concepts.png';
+
+function Header(){
+  const reactDescription = ['Fundamental', 'Crucial', 'Core', 'Critical']
+  function genRandomInt(max){
+    return Math.floor(Math.random() * (max+1));
+  }
   return (
-    <div>
-      <header>
-        <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+    <header>
+        <img src={reactImg} alt="Stylized atom" />
         <h1>React Essentials</h1>
         <p>
-          Fundamental React concepts you will need for almost any app you are
+          {reactDescription[genRandomInt(3)]} React concepts you will need for almost any app you are
           going to build!
         </p>
       </header>
+  );
+}
+
+function App() {
+  return (
+    <div>
+      <Header />
       <main>
         <h2>Time to get started!</h2>
       </main>
